eada55c6b1ec2bd85e7990afca6d7188c41a4afa
Alexis Huerta <alehuer90@gmail.com>
2022-01-25 20:46:33 -0800

    setup initial mikro-orm and postgress connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello there");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import mikroConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig); // connect to DB
+  await orm.getMigrator().up(); // run migrations
+
+  // this simply creates an instance of a Post, but doesn't add to DB
+  //   const post = orm.em.create(Post, { title: "my first post" });
+  //   await orm.em.persistAndFlush(post); // run SQL
+
+  //   const posts = await orm.em.find(Post, {});
+  //   console.log(posts);
+};
+main().catch((err) => {
+  console.error(err);
+});
